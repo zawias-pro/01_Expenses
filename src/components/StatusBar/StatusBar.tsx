fa663b0dbffbdc9e8f44d83786e707f92125afd1
zawias-pro <github@zawias.pro>
2026-06-26 22:18:27 +0200

    status bar simplification

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -9,20 +9,22 @@ const StatusBar = () => {
   const categories = useStore((state) => Object.values(state.categoryMetadata).length)
   const transactions = useStore((state) => state.transactions)
   const transactionsWithoutCategory = transactions.filter(t => t.category === null)
-  const transactionsWithoutCategoryShare = Math.round((transactionsWithoutCategory.length / transactions.length) * 100)
   const totalAmount = Math.abs(transactions.reduce((acc, curr) => acc + Math.min(curr.amount, 0), 0))
   const totalAmountNoCategory = Math.abs(transactionsWithoutCategory.reduce((acc, curr) => acc + Math.min(curr.amount, 0), 0))
-  const totalAmountNoCategoryShare = Math.round((totalAmountNoCategory / totalAmount) * 100)
+  const transactionsWithoutCategoryShare = transactions.length === 0 ? 0 : Math.round((transactionsWithoutCategory.length / transactions.length) * 100)
+  const totalAmountNoCategoryShare = totalAmount === 0 ? 0 : Math.round((totalAmountNoCategory / totalAmount) * 100)
 
   return (
     <div className={styles['status-bar']}>
       <div>Categories: {categories}</div>
-      <div>Transactions: {transactions.length}</div>
-      <div>Transactions without category: {transactionsWithoutCategory.length} ({transactionsWithoutCategoryShare}%)</div>
+      <div>
+        Transactions: {transactions.length}{' '}
+        ({transactionsWithoutCategoryShare}% without category)
+      </div>
       <div>
         Total amount:{' '}
         {formatAmount(totalAmount)}{' '}
-        ({formatAmount(totalAmountNoCategory)} / {totalAmountNoCategoryShare}% without category)
+        ({totalAmountNoCategoryShare}% without category)
       </div>
     </div>
   )
