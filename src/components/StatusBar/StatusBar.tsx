60ccdb97cb6046b874974b45001037428bc57391
zawias-pro <github@zawias.pro>
2026-06-26 22:14:23 +0200

    fix transactions without category display

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -2,33 +2,24 @@ import { useStore } from "../../store/useStore.ts"
 import styles from './StatusBar.module.css'
 
 const formatAmount = (amount: number) => (
-  Intl.NumberFormat(undefined, {style:'currency', currency: 'PLN'}).format(amount)
+  Intl.NumberFormat(undefined, { style: 'currency', currency: 'PLN' }).format(amount)
 )
 
 const StatusBar = () => {
   const categories = useStore((state) => Object.values(state.categoryMetadata).length)
-  const transactions = useStore((state) => state.transactions.length)
-  const transactionsWithoutCategory = useStore((state) => {
-    return state.transactions.filter(t=>t.category===null).length
-  })
-  const transactionsWithoutCategoryShare = Math.round((transactions/transactionsWithoutCategory)*100)
-  const totalAmount = useStore((state) => {
-    return state.transactions.reduce((acc, curr) => {
-      return acc + Math.max(curr.amount, 0)
-    }, 0)
-  })
-  const totalAmountNoCategory = useStore((state) => {
-    return state.transactions.filter(t=>t.category===null).reduce((acc, curr) => {
-      return acc + Math.max(curr.amount, 0)
-    }, 0)
-  })
+  const transactions = useStore((state) => state.transactions)
+  const transactionsWithoutCategory = transactions.filter(t => t.category === null)
+  const transactionsWithoutCategoryShare = Math.round((transactionsWithoutCategory.length / transactions.length) * 100)
+  const totalAmount = transactions.reduce((acc, curr) => acc + Math.max(curr.amount, 0), 0)
+  const totalAmountNoCategory = transactionsWithoutCategory.reduce((acc, curr) => acc + Math.max(curr.amount, 0), 0)
+  // todo: for some reason always displays 100%
   const totalAmountNoCategoryShare = Math.round((totalAmountNoCategory / totalAmount) * 100)
 
   return (
     <div className={styles['status-bar']}>
       <div>Categories: {categories}</div>
-      <div>Transactions: {transactions}</div>
-      <div>Transactions without category: {transactionsWithoutCategory} ({transactionsWithoutCategoryShare}%)</div>
+      <div>Transactions: {transactions.length}</div>
+      <div>Transactions without category: {transactionsWithoutCategory.length} ({transactionsWithoutCategoryShare}%)</div>
       <div>
         Total amount:{' '}
         {formatAmount(totalAmount)}{' '}
